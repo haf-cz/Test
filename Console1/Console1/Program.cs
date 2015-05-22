e837e288f823303cd16d0aecdbafc89037203a37
haf-cz <josefkafka1@gmail.com>
2015-05-22 16:45:28 +0200

    initial version

---
+++ Console1/Console1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Jan = new Person("Jan", "Novak", new DateTime(1987, 11, 19));
+            Console.WriteLine("Press Enter to end");
+            Console.ReadLine();
+        }
+
+        private class Person
+        {
+             public Person(string FirstName, string v2, DateTime DateOfBirth)
+            {
+                this.FirstName = FirstName;
+                this.LastName = v2;
+                this.DateOfBirth = DateOfBirth;
+            }
+
+            public DateTime DateOfBirth { get; internal set; }
+            public string FirstName { get; internal set; }
+            public string LastName { get; internal set; }
+
+            public List<Person> Childrens { get; set; }
+
+            public int Age
+            {
+                get
+                {
+                    return (DateTime.Now - this.DateOfBirth).Days / 365;
+                }
+            }
+
+            public override string ToString() => $"{FirstName} {LastName} is {Age}";
+        }
+    }
+}
